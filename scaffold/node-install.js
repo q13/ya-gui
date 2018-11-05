04c80485dcf534b95d5d6b2933e489c10844310c
q13 <qsimon13@gmail.com>
2018-11-05 12:11:29 +0800

    Test

--- scaffold/node-install.js
+++ scaffold/node-install.js
@@ -3,6 +3,7 @@
  */
 const path = require('path');
 const os = require('os');
+const fsExtra = require('fs-extra');
 const {
   spawnSync
 } = require('child_process');
@@ -15,15 +16,17 @@ const {
 const osType = os.type();
 
 const uri = getNodeLibUri();
+const libName = getNodeLibName();
 const distPath = path.resolve(__dirname, '../lib/nodejs');
 console.log('Node download url is', uri);
+// Remove old pkg
+fsExtra.removeSync(path.resolve(distPath, `./${libName}`));
 download(uri, distPath, {
   extract: true
 }).then(() => {
   console.log('Download done');
   // macOS下设置权限777
   if (osType === 'Darwin') {
-    const libName = getNodeLibName();
     const libDir = path.resolve(distPath, `./${libName}/bin`);
     const output = spawnSync('chmod', ['-R', '777', libDir]);
     if (output.status === 0) {
